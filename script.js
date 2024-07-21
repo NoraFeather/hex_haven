bf3fed827a75ce1b70a1c7bedbd6be1d33451b9c
NoraFeather <114102737+NoraFeather@users.noreply.github.com>
2024-07-21 17:07:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById('signupForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('http://localhost:5000/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    });
+    
+    const result = await response.json();
+    alert(result.message);
+});
+
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    });
+
+    const result = await response.json();
+    if (result.success) {
+        alert('Connexion réussie!');
+    } else {
+        alert(result.message);
+    }
+});
